2d71483bd96ca3682777be12999edc2d6a8b2701
tinggggT <ting940223@gmail.com>
2024-03-11 20:41:25 +0800

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld_03._11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            txtShow.Text = "Hello World!"; //你可以直接複製貼過去
+        }
+    }
+}
